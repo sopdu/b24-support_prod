b4432a69dd2816de6025e77ef08be403482ec81c
alexey <av@inalytics.ru>
2019-02-27 19:05:18 +0300

    regUser::getMaxIdLead (private)

--- local/class/TicketTask.php
+++ local/class/TicketTask.php
@@ -99,5 +99,13 @@
 				return;
 			}
 		}
+		
+		private function getMaxIdLead(){
+			global $DB;
+			$zapros = $DB->Query("
+            SELECT * FROM b_crm_lead WHERE ID=(SELECT MAX(ID) FROM b_crm_lead);
+        ");
+			return $zapros->Fetch();
+		}
 	}
 ?>

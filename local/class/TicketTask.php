90d5524d4ad4255f9c37d714ff863bf34ffd904d
alexey <av@inalytics.ru>
2019-02-27 17:42:00 +0300

    Подключение необдобимых модулей в файле классов

--- local/class/TicketTask.php
+++ local/class/TicketTask.php
@@ -1,7 +1,15 @@
 <?php
-	/**
-	 * Created by PhpStorm.
-	 * User: alexey
-	 * Date: 2019-02-27
-	 * Time: 17:36
-	 */
+	/** Подключаем необходимые битриксовские модули */
+	CModule::IncludeModule("tasks"); // модуль задачи
+	CModule::IncludeModule("support"); // модель техническая поддержка
+	CModule::IncludeModule("forum"); // модуль форум
+	CModule::IncludeModule("socialnetwork"); // модуль социальная сеть
+	CModule::IncludeModule("blog"); // модуль блог
+	CModule::IncludeModule("im"); // модуль мессенджера (бизнес-чат)
+	CModule::IncludeModule("main"); // главный модуль
+	CModule::IncludeModule("iblock"); // модуль информационных блоков
+	CModule::IncludeModule("crm"); // модуль CRM
+	use Bitrix\Main\Mail\Event; // модуль почтовых событий
+	
+	
+?>
